71c3c0fbf6afe91f938b096c3cd422d44048463d
alevarriola <alev_arriola@hotmail.com>
2025-07-19 14:34:47 -0300

    modificado el archivo personaje

--- personajes.py
+++ personajes.py
@@ -4,21 +4,37 @@ from entidad import Entidad
 class Heroe(Entidad):
     def __init__(self, mapa):
         self.mapa = mapa
+        self.poderes = {
+            "speed": False,
+            "salto": False,
+            "teleport": False,
+            "escudo": False,
+        }
 
     def movimiento(self, direccion):
         if direccion == "w":
-            self.pos_y -= 1
+            if self.mapa.es_valido(self.pos_x, self.pos_y - 1):
+                self.y -= 1
         elif direccion == "s":
-            self.pos_y += 1
+            if self.mapa.es_valido(self.pos_x, self.pos_y + 1):
+                self.y += 1
         elif direccion == "a":
-            self.pos_x -= 1
+            if self.mapa.es_valido(self.pos_x, self.pos_y - 1):
+                self.x -= 1
         elif direccion == "d":
-            self.pos_x += 1
+            if self.mapa.es_valido(self.pos_x, self.pos_y + 1):
+                self.x += 1
 
 
 class Villanos(Entidad):
     def __init__(self, mapa):
         self.mapa = mapa
+        self.poderes = {
+            "speed": False,
+            "salto": False,
+            "teleport": False,
+            "escudo": False,
+        }
 
-    def movimiento(self, direccion):
+    def movimiento(self):
         movimientos = [(1, 0), (-1, 0), (0, 1), (0, -1)]
