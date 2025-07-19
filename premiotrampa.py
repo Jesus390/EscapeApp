65d3f6242d4a3efd8b4fd8a1c6756a5b47a915d0
alevarriola <alev_arriola@hotmail.com>
2025-07-19 15:07:22 -0300

    modificar premiosytrampas

--- premiotrampa.py
+++ premiotrampa.py
@@ -1,13 +1,13 @@
 from entidad import Entidad
-
+import random 
 #clase para los tipos de poderes(speed,saltar muros,teleport,escudo Antivillano(Exclusivo para el personaje))
 class Premio(Entidad):
   
-    def __init__(self, x, y, tipo, exclusivo_jugador=False):
-        
+    def __init__(self, x, y, tipo, mapa, exclusivo_jugador=False):
+
+        self.mapa = mapa
         super().__init__(x, y)
 
-       
         self.tipo = tipo
 
         # si es un premio exclusivo del jugador, como el escudo
@@ -37,5 +37,30 @@ class Premio(Entidad):
         elif self.tipo == "escudo":
             if self.exclusivo_jugador and quien_usa.es_jugador:
                 quien_usa.poderes["escudo"] = True
+
+#clase para posicionar los premios/poderes en el mapa 
+class PosicionarPoderes:
+    def __init__(self,mapa):
+        self.mapa = mapa
+        self.premios = []
+
+    def generar_poder(self,cantidad):
+        tipos = ["speed", "saltar", "teleport", "escudo"]
+        filas = len(self.mapa)
+        columnas = len(self.mapa[0])
+
+       
+        for _ in range(cantidad):
+            tipo = random.choice(tipos)
+            x = random.randint(0, columnas - 1)
+            y = random.randint(0, filas - 1)
+            #creo el objeto con tu tipo y posicion
+            premio = Premio(x, y, tipo)
+            self.mapa[y][x] = premio.get_valor() #posiciono el poder en el mapa
+            self.premios.append(premio) #guardo para usar despues
+
+
+
+
     
     
