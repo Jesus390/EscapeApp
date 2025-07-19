6c59e4c07f639eafb80e37d084f368183018dd1f
Lujan <lujanmelga25@gmail.com>
2025-07-19 12:05:45 -0300

    premio-personajes

--- premiotrampa.py
+++ premiotrampa.py
@@ -1,6 +1,7 @@
-class Trampa:
-    def __init__(self):
-        pass
-class Premio:
-    def __init__(self):
-        pass
+#clase para los tipos de poderes(speed,saltar muros,teleport,escudo Antivillano(Exclusivo para el personaje))
+class Premio(Entidad):
+    def __init__(self, x, y, tipo, exclusivo_jugador=False):
+        self.x = 
+        self.y = 
+        self.tipo =
+        
